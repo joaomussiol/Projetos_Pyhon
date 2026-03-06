929e8cd7053074fc1258135b672b1e8dd8e40c1a
Joao Mussiol <joao_mussiol@estudante.sesisenai.org.br>
2026-03-05 21:08:09 -0300

    comeco do teste de idade de cachorro

---
+++ idade_cachorro.py
@@ -0,0 +1,4 @@
+dog_name = input("diga o nome do seu dogo aí: ")
+old_dog_age = float(input("diga a idade do teu dogo aí: "))
+new_dog_age = float(old_dog_age * 7)
+print("nome do teu dogo é:",dog_name + " e ele tem ",new_dog_age,"anos")
