f529654231d79c93f013cfea4a45243fd7bbee24
Joao Mussiol <joao_mussiol@estudante.sesisenai.org.br>
2026-03-17 14:17:27 -0300

    idade cachorro arrumada

--- idade_cachorro.py
+++ idade_cachorro.py
@@ -1,4 +1,8 @@
-dog_name = input("diga o nome do seu dogo aí: ")
-old_dog_age = float(input("diga a idade do teu dogo aí: "))
+print("Vamos calcular a idade do seu cachorro em idade humana")
+dog_name = input("Então me diga o nome do seu doguinho aí: ")
+old_dog_age = float(input("e quantos anos o seu doguinho tem? "))
 new_dog_age = float(old_dog_age * 7)
-print("nome do teu dogo é:",dog_name + " e ele tem ",new_dog_age,"anos")
+if new_dog_age >= 20:
+    print("seu doguinho não é tão velho assim, diga a idade verdadeira dele aí")
+else:
+    print("Então o seu doguinho ",dog_name + "tem ",new_dog_age,"anos em idade humana.")
