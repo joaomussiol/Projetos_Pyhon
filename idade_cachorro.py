a6ec6ab5b33ea626730c3282b5728563c347ea2d
Joao Mussiol <joao_mussiol@estudante.sesisenai.org.br>
2026-03-17 21:22:08 -0300

    idade de cahorro mudança 1

--- idade_cachorro.py
+++ idade_cachorro.py
@@ -1,8 +1,9 @@
 print("Vamos calcular a idade do seu cachorro em idade humana")
 dog_name = input("Então me diga o nome do seu doguinho aí: ")
 old_dog_age = float(input("e quantos anos o seu doguinho tem? "))
+
 new_dog_age = float(old_dog_age * 7)
-if new_dog_age >= 20:
+if old_dog_age >= 20:
     print("seu doguinho não é tão velho assim, diga a idade verdadeira dele aí")
 else:
-    print("Então o seu doguinho ",dog_name + "tem ",new_dog_age,"anos em idade humana.")
+    print("Então o seu doguinho ",dog_name + " tem",new_dog_age,"anos em idade humana.")
