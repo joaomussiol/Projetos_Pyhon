bf74ce53184672d3ebf14bfe60a8fc82066df8b8
Joao Mussiol <joao_mussiol@estudante.sesisenai.org.br>
2026-03-18 15:33:37 -0300

    Calculadora Versão 3. Agora com f-string e resultado = None

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,25 @@
 num1 = float(input("Digite o primeiro numero: "))
 operador = input("Digite um operador. Ele pode ser. +, -, *, / : ")
 num2 = float(input("Digite o segundo numero: "))
+resultado = None
 
 if operador == "+" :
     resultado = num1 + num2
-    print("Resultado é:",resultado)
 
 elif operador == "-" :
     resultado = num1 - num2
-    print("Resultado é:",resultado)
 
 elif operador == "/" :
-    try:
+    if num2 == 0:
+        print("ERRO, DIVISÃO POR ZERO")
+    else:
         resultado = num1 / num2
-        print("Resultado é:",resultado)
-    except Exception as e:
-        print(e)
 
 elif operador == "*" :
     resultado = num1 * num2
-    print("Resultado é:", resultado)
 
 else:
-    print("operador invalido")
+    print("Operador invalido")
+
+if resultado is not None:
+    print(f"{num1} {operador} {num2} = {resultado}")
